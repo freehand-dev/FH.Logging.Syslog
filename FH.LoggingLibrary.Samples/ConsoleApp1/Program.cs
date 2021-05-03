cfcc6d9868e0a77f1dc0b477beeca1b53b7977b7
Oleksandr Nazaruk <mail@freehand.com.ua>
2021-05-03 21:03:06 +0300

    Microsoft.Extensions.Logging 5.0

--- FH.LoggingLibrary.Samples/ConsoleApp1/Program.cs
+++ FH.LoggingLibrary.Samples/ConsoleApp1/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Net;
-using FH.LoggingLibrary;
 
-namespace FH.LoggingLibrary.Samples
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Sending test message to localhost Syslog server...");
 
             using (Syslog.Client c = new Syslog.Client(IPAddress.Parse("172.17.82.26"), 514, Syslog.Facility.Local6, Syslog.Level.Warning, "Test.Application Test"))
             {
                 c.Send("This is a test of the syslog client code. Привет");
 
-                c.Send(new Syslog.Rfc3164Message(Syslog.Facility.Local6, Syslog.Level.Warning, "Test.Application test", "This is a test of the syslog client code. Привет"){
+                c.Send(new Syslog.Rfc3164Message(Syslog.Facility.Local6, Syslog.Level.Warning, "Test.Application test", "This is a test of the syslog client code. Привет")
+                {
                     PID = 1000
                 });
             }
---
+++ FH.LoggingLibrary.Samples/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using FH.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ILoggerFactory
+            var loggerFactory = LoggerFactory.Create(builder => 
+            {
+                builder
+                    .ClearProviders()
+                    .SetMinimumLevel(LogLevel.Trace)
+                    .AddFilter("Microsoft", LogLevel.Debug)
+                    .AddFilter("System", LogLevel.Debug)
+                    .AddFilter("ConsoleApp2.Program", LogLevel.Debug)
+                    .AddConsole()
+                    .AddSyslog(
+                        new SysLogSettings() 
+                        { 
+                            Host = "172.17.82.26",
+                            Port = 514,
+                        });
+            });
+
+            var _logger = loggerFactory.CreateLogger("Program");
+
+            _logger.LogInformation("LogInformation");
+            _logger.LogDebug("LogDebug");
+            _logger.LogTrace("LogTrace");
+            _logger.LogError("LogError");
+            _logger.LogCritical("LogCritical");
+            _logger.LogWarning("LogWarning");
+        }
+    }
+}
